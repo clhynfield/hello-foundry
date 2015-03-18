862915ad459e93370c13cef9bb2dcfaaf7cb0a9c
Clayton Hynfield <clayton@hynfield.org>
2015-03-18 10:45:08 -0400

    Log each request

--- index.js
+++ index.js
@@ -3,14 +3,17 @@
 // Load the http module to create an http server.
 var http = require('http');
 
+var listenPort = process.env.VCAP_APP_PORT || 8000;
+
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World\n');
+  console.log('Another happy world served!');
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(process.env.VCAP_APP_PORT || 8000);
+server.listen(listenPort);
 
 // Put a friendly message on the terminal
-console.log('Server running at http://127.0.0.1:8000/');
+console.log('Server running on port ' + listenPort);
